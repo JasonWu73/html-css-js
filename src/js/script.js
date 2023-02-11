9552733b9db4de6340b842fbd2dce9843507a4d5
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-11 23:02:26 +0800

    destructing

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,14 @@
-const nums = [2, 3, 4];
-const newNums = [...nums, 5];
-console.log(newNums);
+const nums = [1, 2, 3];
+const [a, , c] = nums;
+console.log(a, c);
 
-console.log('------------------');
+console.log('=====================');
 
 const person = {
-  name: 'Jason'
-};
-const newPerson = {
-  ...person,
+  name: 'Jason Wu',
+  hobby: 'Coding',
   age: 25
 };
 
-console.log(newPerson);
-
-console.log('------------------');
-
-const filterEvenNum = (...args) => args.filter(num => num % 2 === 0);
-console.log(filterEvenNum(1, 2, 3, 4, 5));
+const {name, age} = person;
+console.log(`name=${name}, age=${age}`);
