3c3da8c4321ea78e7b9f2d3ebde6409db3f39b46
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-11 22:29:15 +0800

    understand let and const

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// var myName = '吴仙杰';
+// console.log(myName);
+// myName = 'Jason Wu';
+// console.log(myName);
+
+// let myName = '吴仙杰';
+// console.log(myName);
+// myName = 'Jason Wu';
+// console.log(myName);
+
+const myName = '吴仙杰';
+console.log(myName);
+myName = 'Jason Wu';
+console.log(myName);
