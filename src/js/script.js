9ca55249c4896d9ce415f0aec9ae7666756dd4d6
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-11 23:15:53 +0800

    array method

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,3 @@
-// string, number, boolean is primitive type
-let name = 'Jason';
-let newName = name;
-newName = 'Bruce';
-console.log(`name=${name}, newName=${newName}`);
-
-console.log('=====================');
-
-// object, array is reference type
-let arr = [1, 2, 3];
-let newArr = arr;
-newArr.push(7, 8, 9);
-console.log(`arr=${arr}, newArr=${newArr}`);
-
-// copy value
-let newArr2 = [...arr];
-newArr2.push(11, 12);
-console.log(`arr=${arr}, newArr=${newArr}, newArr2=${newArr2}`);
+const nums = [1, 2, 3];
+const doubleNums = nums.map(num => num * 2);
+console.log(doubleNums);
