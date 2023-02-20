29cfc9af4a24d9cce401a97b1cd51d09ce3e4a32
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-20 16:09:16 +0800

    working with number, string and boolean

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,28 @@
-const num1El = document.querySelector('.num1') as HTMLInputElement;
-const num2El = document.querySelector('.num2')! as HTMLInputElement;
-const btnEl = document.querySelector('button') as HTMLButtonElement;
+const add = (
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  phrase: string
+) => {
+  const sum = n1 + n2;
+  if (showResult) {
+    console.log(`${phrase} ${sum}`);
+  }
 
-btnEl.addEventListener('click', () => {
-  console.log(sum(+num1El.value, +num2El.value));
-});
+  return sum;
+};
 
-function sum(num1: number, num2: number) {
-  return num1 + num2;
-}
+const num1 = 0.1;
+const num2 = 0.9;
+const printResult = true;
+const resultPhrase = 'The result is:';
+add(num1, num2, printResult, resultPhrase);
+
+console.log('==========')
+
+const num = 1.32;
+const str = 'hello';
+const bool = false;
+console.log(`typeof ${num}: ${typeof num}`);
+console.log(`typeof ${str}: ${typeof str}`);
+console.log(`typeof ${bool}: ${typeof bool}`);
