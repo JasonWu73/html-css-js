7dfc6037163f6c9d94c65e2803716520b554c9a7
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-20 14:42:48 +0800

    set up ts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,14 +1,11 @@
-class Person {
+const num1El = document.querySelector('.num1') as HTMLInputElement;
+const num2El = document.querySelector('.num2')! as HTMLInputElement;
+const btnEl = document.querySelector('button') as HTMLButtonElement;
 
-  constructor(public name: string, public age: number) {
-    this.name = name;
-    this.age = age;
-  }
+btnEl.addEventListener('click', () => {
+  console.log(sum(+num1El.value, +num2El.value));
+});
 
-  printPersonData() {
-    console.log(`Name is ${this.name}, age is ${this.age}`)
-  }
+function sum(num1: number, num2: number) {
+  return num1 + num2;
 }
-
-const person = new Person('Jason Wu', 25);
-person.printPersonData();
