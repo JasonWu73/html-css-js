d9bda4379b2ec9f0bbd23ef038af80ae828349a9
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-20 22:15:15 +0800

    unknwon type

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,9 +1,3 @@
-add(1, 2, (result) => {
-  console.log(result);
-  return result;
-});
-
-function add(num1: number, num2: number, cb: (num: number) => void) {
-  const result = num1 + num2;
-  cb(result);
-}
+let userInput: unknown;
+let username: string;
+username = userInput + '';
