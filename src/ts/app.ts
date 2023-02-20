af80fcf82cde8eeb115984d7705c8a7e086591d5
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-20 17:16:18 +0800

    enum type

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,19 +1,19 @@
-// object type
-const person: {
-  name: string,
-  age: number,
-  hobbies: string[], // array type
-  roles: [number, string] // tuple
-} = {
+// 0, 1, 2
+// enum Role {ADMIN, READ_ONLY}
+
+// enum Role {ADMIN = 10, READ_ONLY = 100}
+
+enum Role {ADMIN = 'admin', READ_ONLY = 100}
+
+const person = {
   name: 'Jason Wu',
   age: 25,
   hobbies: ['Game', 'Coding'],
-  roles: [2, 'author']
+  role: Role.ADMIN
 };
 
-// error
-// person.roles = [1, 'Coder', 'Dev'];
-
-person.roles.push('Tester');
-person.roles[1] = 'Admin';
-console.log(person);
+if (person.role === Role.READ_ONLY) {
+  console.log(`${person.name} is read only!`);
+} else {
+  console.log(`${person.name} can read and write.`);
+}
