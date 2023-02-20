acea36a85b68c70ff9d0b5b3ac97b2e3998ac1ad
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-20 19:25:31 +0800

    literal type

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,17 @@
-const combine = (n1: number | string, n2: number | string) => {
-  if (typeof n1 === 'number' && typeof n2 === 'number') {
-    return n1 + n2;
+const combine = (
+  n1: number | string,
+  n2: number | string,
+  resultConversion: 'as-number' | 'as-string'
+) => {
+  if (typeof n1 === 'number' && typeof n2 === 'number' ||
+    resultConversion === 'as-number') {
+    return +n1 + +n2;
   }
 
   return n1.toString() + n2.toString();
 };
 
-console.log(combine(1, 2));
+console.log(combine(1, 2, 'as-number'));
+console.log(combine('1', '2', 'as-number'));
 
-console.log(combine('hello', ' world'));
+console.log(combine('hello', ' world', 'as-string'));
