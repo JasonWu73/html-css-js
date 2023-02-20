183cf459ea85497c637be010cbb8862baa944eb7
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-20 22:07:52 +0800

    function type: callback type

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,14 +1,9 @@
-// let myFunc: Function;
-let myFunc: (a: number, b: number) => number;
-myFunc = add;
-// myFunc = printResult;
-// myFunc = 1;
-console.log(myFunc(1, 2));
+add(1, 2, (result) => {
+  console.log(result);
+  return result;
+});
 
-function printResult(num: number) {
-  console.log(`Result: ${num}`);
-}
-
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number, num2: number, cb: (num: number) => void) {
+  const result = num1 + num2;
+  cb(result);
 }
