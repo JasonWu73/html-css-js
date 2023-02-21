06b9aaa5a0c4cbdba44d1c31d44aa63a1a5a650d
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-21 16:32:17 +0800

    first class

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,89 +1,10 @@
-const resultErrorArray = [100, new Error('Error'), 'one', 'two'];
-const [result, error, ...remaining] = resultErrorArray;
-console.log(result);
-console.log(error);
-console.log(remaining);
+class Department {
+  name: string
 
-const person = {
-  username: 'Jason',
-  age: 25
-};
-
-const username = 'Bruce';
-const {age, username: myName} = person;
-console.log(age);
-console.log('username', username);
-console.log('myName', myName);
-
-/*
-const addTwo = (...numbers: [number, number]): number => {
-  return numbers[0] + numbers[1];
-};
-
-console.log(addTwo(1, 2));
-
-const add = (...numbers: number[]): number => {
-  return numbers.reduce((previousValue, currentValue) => {
-    return previousValue + currentValue;
-  }, 0);
-};
-
-const printToConsole = (
-  ...params: (string | number)[]): [string, number] => {
-  // ...params: (string | number)[]): (string | number)[] => {
-  console.log(...params)
-  return ['1000', 1];
-  // return [1, '1000'];
-};
-
-const [
-  stringValue,
-  numberValue] = printToConsole('Hello', 'TypeScript', ':)', 100);
-console.log(stringValue, numberValue);
-*/
-
-/* const hobbies = ['Game', 'Coding'];
-const activeHobbies = ['Fitness'];
-
-// hobbies.push(activeHobbies[0]);
-hobbies.push(...activeHobbies);
-console.log(hobbies);
-
-const person = {
-  name: 'person',
-  age: 25
-};
-
-const jason = {...person, name: 'Jason'};
-console.log(person); */
-
-/*
-const add = (
-  numberOne: number,
-  numberTwo: number = 100): number => numberOne + numberTwo;
-
-const printOutput = (output: number | string) => console.log(output);
-
-const buttonElement = document.querySelector('button');
-if (buttonElement) {
-  buttonElement.addEventListener('click', (event) => {
-    console.log(event.target);
-    printOutput(add(0.1, 0.9));
-  });
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
-console.log(add(1));
-*/
-
-/*
-const age = 25;
-
-if (age > 18) {
-  // let, const: function, global, block scope
-  // var: function, global
-  let ok = true;
-  console.log(ok);
-}
-
-console.log(ok)
-*/
+const accountingDepartment = new Department('Accounting');
+console.log(accountingDepartment);
