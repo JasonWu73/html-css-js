c61d8b9ae9100a6d141df59c62a85979aceab769
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-21 14:56:31 +0800

    arrow function

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,3 +1,20 @@
+const add = (
+  numberOne: number,
+  numberTwo: number): number => numberOne + numberTwo;
+
+console.log(add(0.1, 0.9));
+
+const printOutput = (output: number | string) => console.log(output);
+printOutput(add(0.1, 0.9));
+
+const buttonElement = document.querySelector('button');
+if (buttonElement) {
+  buttonElement.addEventListener('click', (event) => {
+    console.log(event.target);
+  });
+}
+
+/*
 const age = 25;
 
 if (age > 18) {
@@ -8,3 +25,4 @@ if (age > 18) {
 }
 
 console.log(ok)
+*/
