3d73c97e878933f078da11a38ffbaf27e48b7e46
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-21 15:00:54 +0800

    default function parameters

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,18 +1,18 @@
 const add = (
   numberOne: number,
-  numberTwo: number): number => numberOne + numberTwo;
-
-console.log(add(0.1, 0.9));
+  numberTwo: number = 100): number => numberOne + numberTwo;
 
 const printOutput = (output: number | string) => console.log(output);
-printOutput(add(0.1, 0.9));
 
 const buttonElement = document.querySelector('button');
 if (buttonElement) {
   buttonElement.addEventListener('click', (event) => {
     console.log(event.target);
+    printOutput(add(0.1, 0.9));
   });
 }
+
+console.log(add(1));
 
 /*
 const age = 25;
