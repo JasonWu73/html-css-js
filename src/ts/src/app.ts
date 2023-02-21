e320b6e61af9403a955cd1b0ac9307aba453b4b2
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-21 13:33:39 +0800

    strict mode: unused local variable, unused parameter, explicitly return

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,10 +1,24 @@
 const buttonElement = document.querySelector('button')!;
+let appId = 'appid';
 
-const clickHandler = (message: string) => {
+const add = (
+  numberOne: number,
+  numberTwo: number): number | undefined => {
+  const result = numberOne + numberTwo;
+  if (result > 0) {
+    return result;
+  }
+  return;
+};
+
+const clickHandler = (message: string/* , age: number */) => {
+  // let username = 'Jason';
   console.log(`Clicked: ${message}`)
 };
 
-buttonElement.addEventListener('click', clickHandler.bind(null, 'data'));
+buttonElement.addEventListener(
+  'click',
+  clickHandler.bind(null, 'data', 25));
 
 let logged;
 
