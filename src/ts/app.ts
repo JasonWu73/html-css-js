8bd2ca2b623f866fea52cec7195e29835fcfb312
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-21 09:46:13 +0800

    typescript configuration listen directory, watch mode

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,12 +1,1 @@
-genError(500, 'deliberate throw error')
-console.log('finish')
-
-const test = (a: string): void => {
-
-};
-
-function genError(
-  code: number,
-  msg: string): never {
-  throw {code, msg};
-}
+console.log('typescript watch mode, changed')
