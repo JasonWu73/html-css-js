0a18c2a9abf6632afc5f03c01a5f5ceb30f76aba
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-22 12:47:52 +0800

    discriminated union

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,64 +1,29 @@
-// type guard
-type numberOrString = number | string;
-type numeric = number | boolean;
-type combined = numberOrString | numeric;
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
 
-const add = (a: combined, b: combined): number | string => {
-  if (typeof a === 'string' || typeof b === 'string') {
-    return a.toString() + b.toString();
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const moveAnimal = (animal: Animal): void => {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
   }
 
-  return +a + +b;
+  console.log(`Moving at speed: ${speed}`);
 };
 
-console.log('add(\'1\', 1): ', add('1', 1));
-console.log('add(1, false)', add(1, false));
-console.log('add(1, true)', add(1, true));
+const sparrow: Bird = {type: 'bird', flyingSpeed: 100};
+moveAnimal(sparrow);
 
-interface Animal {
-  name: string;
-}
-
-interface People {
-  gender: string;
-}
-
-type Account = Animal | People;
-
-class MyAnimal implements Animal {
-
-  constructor(public name: string) {
-  }
-}
-
-class MyPeople implements People {
-
-  constructor(public gender: string) {
-  }
-}
-
-const printUserInfo = (user: Account): void => {
-  // in keyword can check if object property exists
-  // if ('name' in user) {
-  //   console.log(`Name: ${user.name}`);
-  // }
-  //
-  // if ('gender' in user) {
-  //   console.log(`Gender: ${user.gender}`);
-  // }
-
-  // instance keyword also can check if object property exists
-  if (user instanceof MyAnimal) {
-    console.log(`Name: ${user.name}`);
-  }
-
-  if (user instanceof MyPeople) {
-    console.log(`Gender: ${user.gender}`);
-  }
-}
-
-// printUserInfo({name: 'Jason'});
-// printUserInfo({gender: 'female'});
-
-printUserInfo(new MyAnimal('Cat'));
-printUserInfo(new MyPeople('Male'))
+moveAnimal({type: 'horse', runningSpeed: 200});
