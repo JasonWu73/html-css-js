dae1b4b2e8cf0ccb1c32c4ddf526c8164b771fba
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-22 12:58:36 +0800

    type casting

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,29 +1,8 @@
-interface Bird {
-  type: 'bird';
-  flyingSpeed: number;
-}
+// type casting
+// const paragraphElement = <HTMLInputElement>
+//   document.querySelector('.message-output')!;
 
-interface Horse {
-  type: 'horse';
-  runningSpeed: number;
-}
+const paragraphElement =
+  document.querySelector('.message-output')! as HTMLInputElement;
 
-type Animal = Bird | Horse;
-
-const moveAnimal = (animal: Animal): void => {
-  let speed;
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed;
-      break;
-    case 'horse':
-      speed = animal.runningSpeed;
-  }
-
-  console.log(`Moving at speed: ${speed}`);
-};
-
-const sparrow: Bird = {type: 'bird', flyingSpeed: 100};
-moveAnimal(sparrow);
-
-moveAnimal({type: 'horse', runningSpeed: 200});
+paragraphElement.value = 'Hi there!';
