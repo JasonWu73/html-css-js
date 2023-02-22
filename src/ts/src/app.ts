2f6b1ca9c6a9c441a0191b118fd14f8bbee2a1f8
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-22 11:52:50 +0800

    type guard

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,13 +1,19 @@
-// intersection type
+// type guard
 type numberOrString = number | string;
-type stringOrBoolean = string | boolean;
-type combined = numberOrString & stringOrBoolean;
+type numeric = number | boolean;
+type combined = numberOrString | numeric;
 
-let variable: combined;
+const add = (a: combined, b: combined): number | string => {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
 
-variable = '';
-// variable = false; // error
-// variable = 100; // error
+  return +a + +b;
+};
+
+console.log('add(\'1\', 1): ', add('1', 1));
+console.log('add(1, false)', add(1, false));
+console.log('add(1, true)', add(1, true));
 
 interface Animal {
   name: string;
@@ -17,13 +23,42 @@ interface People {
   gender: string;
 }
 
-type AccountUser = Animal & People;
+type Account = Animal | People;
 
-let user: AccountUser;
+class MyAnimal implements Animal {
 
-user = {
-  name: 'Jason',
-  gender: 'male'
-};
+  constructor(public name: string) {
+  }
+}
 
-console.log(user);
+class MyPeople implements People {
+
+  constructor(public gender: string) {
+  }
+}
+
+const printUserInfo = (user: Account): void => {
+  // in keyword can check if object property exists
+  // if ('name' in user) {
+  //   console.log(`Name: ${user.name}`);
+  // }
+  //
+  // if ('gender' in user) {
+  //   console.log(`Gender: ${user.gender}`);
+  // }
+
+  // instance keyword also can check if object property exists
+  if (user instanceof MyAnimal) {
+    console.log(`Name: ${user.name}`);
+  }
+
+  if (user instanceof MyPeople) {
+    console.log(`Gender: ${user.gender}`);
+  }
+}
+
+// printUserInfo({name: 'Jason'});
+// printUserInfo({gender: 'female'});
+
+printUserInfo(new MyAnimal('Cat'));
+printUserInfo(new MyPeople('Male'))
