a2a852bb9e121dec77725ebd6b4cfd1a7a905a4d
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-22 13:26:38 +0800

    function overloads

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,10 +1,20 @@
-// index properties
-interface ErrorContainer {
-  [property: string]: string;
+// function overloads
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable): string | number {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString;
+  }
+
+  return a + b;
 }
 
-const errorBag: ErrorContainer = {
-  'email': 'Invalid Email Address',
-  1: 'Fatal'
-};
-console.log(errorBag);
+const total = add('Hello', 1);
+console.log(total.trim());
