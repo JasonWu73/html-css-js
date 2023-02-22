b1289b3739ed4aedc7eb5244a6bf633bbe956b35
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-22 13:53:00 +0800

    optional chaining

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,20 +1,25 @@
+// optional chaining
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json?.notExists?.id))
+
 // function overloads
-type Combinable = string | number;
-type Numeric = number | boolean;
-
-type Universal = Combinable & Numeric;
-
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: string, b: number): string;
-function add(a: number, b: string): string;
-function add(a: Combinable, b: Combinable): string | number {
-  if (typeof a === 'string' || typeof b === 'string') {
-    return a.toString() + b.toString;
-  }
-
-  return a + b;
-}
-
-const total = add('Hello', 1);
-console.log(total.trim());
+// type Combinable = string | number;
+// type Numeric = number | boolean;
+//
+// type Universal = Combinable & Numeric;
+//
+// function add(a: number, b: number): number;
+// function add(a: string, b: string): string;
+// function add(a: string, b: number): string;
+// function add(a: number, b: string): string;
+// function add(a: Combinable, b: Combinable): string | number {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString;
+//   }
+//
+//   return a + b;
+// }
+//
+// const total = add('Hello', 1);
+// console.log(total.trim());
