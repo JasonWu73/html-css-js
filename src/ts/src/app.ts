76e44beb553ed98d86d31528757d43f0e0d62404
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-22 14:31:07 +0800

    built-in generic

--- src/ts/src/app.ts
+++ src/ts/src/app.ts
@@ -1,35 +1,13 @@
-// nullish coalescing
-// const data = undefined;
-// const data = null;
-const data = 0;
-const result = data ?? 'DEFAULT';
-// const result = data || 'DEFAULT';
-console.log(result);
+// built-in generic
+const names: Array<string> = []; // string[]
+// names[0].toUpperCase()
 
-console.log((void 0) === undefined);
+const promise: Promise<string> = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('This is done!');
+  }, 2000);
+});
 
-// optional chaining
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   .then(response => response.json())
-//   .then(json => console.log(json?.notExists?.id))
-
-// function overloads
-// type Combinable = string | number;
-// type Numeric = number | boolean;
-//
-// type Universal = Combinable & Numeric;
-//
-// function add(a: number, b: number): number;
-// function add(a: string, b: string): string;
-// function add(a: string, b: number): string;
-// function add(a: number, b: string): string;
-// function add(a: Combinable, b: Combinable): string | number {
-//   if (typeof a === 'string' || typeof b === 'string') {
-//     return a.toString() + b.toString;
-//   }
-//
-//   return a + b;
-// }
-//
-// const total = add('Hello', 1);
-// console.log(total.trim());
+promise.then(data => {
+  data.toUpperCase();
+});
