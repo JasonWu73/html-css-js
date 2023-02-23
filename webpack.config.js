e8d4e615d6ad55b4d9c4ae1475ae6f568964f517
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-23 13:40:46 +0800

    配置 Webpack 开发服务器，HTML 及 CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,46 @@
+// npm install --save-dev html-webpack-plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// npm install --save-dev mini-css-extract-plugin
+// npm install --save-dev css-loader
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/ts/src/app.ts',
+  mode: 'development', // 打包生产环境代码则使用 `production`
+  entry: './src/ts/src/ts/app.ts',
   devtool: 'inline-source-map', // `tsconfig.json`: `"sourceMap": true`
   output: {
-    filename: 'bundle.js', // 可加 Hash 值：[contenthash]
-    path: path.resolve(__dirname, 'src/ts/dist') // 必需为绝对路径
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'src/ts/dist'), // 必需为绝对路径
+    clean: true
   },
+  devServer: {
+    // http://[devServer.host]:[devServer.port]/[output.publicPath]/[output.filename]
+    static: [
+      {
+        directory: path.resolve(__dirname, 'src/ts/css'),
+        publicPath: '/dist/css/'
+      }
+    ],
+    port: 3000,
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/ts/src/test-ts.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
