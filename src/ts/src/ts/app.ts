19365fe1a7d7489a8e575ed1b38da246b0b7de6c
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-23 16:18:48 +0800

    class validation

--- src/ts/src/ts/app.ts
+++ src/ts/src/ts/app.ts
@@ -1,22 +1,30 @@
 import 'reflect-metadata';
+import { plainToInstance } from "class-transformer";
 
 import '../css/style.css';
-
-import { plainToInstance } from "class-transformer";
 import { Post } from "./post.model";
+import { validate } from "class-validator";
 // import { plainPostMapper } from "./post.model";
 
 getPostsFromJsonPlaceHolder()
   .then(posts => {
-    posts.forEach((post, index) =>
-      console.log(`${index + 1}. ${post.getTitleInformation()}`));
+    posts.forEach((post, index) => {
+      validate(post).then(errors => {
+        if (errors.length > 0) {
+          console.log('validation failed. errors: ', errors);
+          return;
+        }
+
+        console.log(`${index + 1}. ${post.getTitleInformation()}`);
+      });
+    });
   });
 
 async function getPostsFromJsonPlaceHolder(): Promise<Post[]> {
   const response = await fetch(
     'https://jsonplaceholder.typicode.com/posts?userId=1'
   );
-  const posts: Post[] = await response.json();
+  const posts = await response.json();
 
   // return posts.map(post => plainPostMapper(post));
 
--- src/ts/src/ts/post.model.ts
+++ src/ts/src/ts/post.model.ts
@@ -1,11 +1,17 @@
-export class Post {
+import { IsEmpty } from "class-validator";
 
-  constructor(
-    public userId: number,
-    public id: number,
-    public title: string,
-    public body: string
-  ) {
+export class Post {
+  userId: number;
+  public id: number;
+  public title: string;
+  @IsEmpty({ message: "内容必须为空" })
+  public body: string;
+
+  constructor(userId: number, id: number, title: string, body: string) {
+    this.userId = userId;
+    this.id = id;
+    this.title = title;
+    this.body = body;
   }
 
   getTitleInformation(): string {
