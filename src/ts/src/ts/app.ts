cd1989fa790069fd165d32bdb415294a3ac5e69b
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-23 21:00:40 +0800

    first class decorator

--- src/ts/src/ts/app.ts
+++ src/ts/src/ts/app.ts
@@ -1,32 +1,18 @@
-import 'reflect-metadata';
-import { plainToInstance } from "class-transformer";
-
 import '../css/style.css';
-import { Post } from "./post.model";
-import { validate } from "class-validator";
-// import { plainPostMapper } from "./post.model";
 
-getPostsFromJsonPlaceHolder()
-  .then(posts => {
-    posts.forEach((post, index) => {
-      validate(post).then(errors => {
-        if (errors.length > 0) {
-          console.log('validation failed. errors: ', errors);
-          return;
-        }
-
-        console.log(`${index + 1}. ${post.getTitleInformation()}`);
-      });
-    });
-  });
-
-async function getPostsFromJsonPlaceHolder(): Promise<Post[]> {
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?userId=1'
-  );
-  const posts = await response.json();
-
-  // return posts.map(post => plainPostMapper(post));
-
-  return plainToInstance(Post, posts);
+function Logger(constructor: Function) {
+  console.log('Logging...')
+  console.log('constructor: ', constructor);
 }
+
+@Logger
+class Person {
+  name = 'Jason';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const person = new Person();
+console.log(person);
