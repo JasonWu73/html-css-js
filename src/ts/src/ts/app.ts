e3e5ea81881f105f8888e54b39d321f4a69f5822
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-23 21:35:49 +0800

    decorator order of execution

--- src/ts/src/ts/app.ts
+++ src/ts/src/ts/app.ts
@@ -1,14 +1,18 @@
 import '../css/style.css';
 
 function Logger(logMessage: string) {
+  console.log('LOGGER FACTORY');
   return (constructor: Function) => {
+    console.log('Logger');
     console.log(logMessage);
     console.log('constructor: ', constructor);
   };
 }
 
 const withTemplate = (template: string, hookId: string) => {
+  console.log('WITH_TEMPLATE FACTORY');
   return (constructor: any) => {
+    console.log('Render Template');
     const hookedElement = document.getElementById(hookId);
     if (hookedElement) {
       const person = new constructor();
@@ -18,7 +22,7 @@ const withTemplate = (template: string, hookId: string) => {
   };
 };
 
-// @Logger('LOGGING - PERSON')
+@Logger('LOGGING - PERSON')
 @withTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Jason';
