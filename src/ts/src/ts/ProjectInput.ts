130634d8e61479a0251a2e2ca2fa4a000d21134e
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-28 18:43:20 +0800

    格式化 as

--- src/ts/src/ts/ProjectInput.ts
+++ src/ts/src/ts/ProjectInput.ts
@@ -10,10 +10,10 @@ class ProjectInput {
   peopleElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement =
-      document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostElement =
-      document.getElementById('app')! as HTMLDivElement;
+    this.templateElement = document.getElementById('project-input')! as
+      HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as
+      HTMLDivElement;
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
