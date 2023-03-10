7259cf0c820b1a75abb7af454dda8ea31ae79472
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-10 16:38:13 +0800

    样式定义

--- src/ts/src/ts/app.ts
+++ src/ts/src/ts/app.ts
@@ -1,9 +1,3 @@
 import '../css/style.css';
 
-import ProjectInput from "./ProjectInput";
-import ProjectList from "./ProjectList";
-import { ProjectStatus } from "./ProjectState";
 
-new ProjectInput();
-new ProjectList(ProjectStatus.Active);
-new ProjectList(ProjectStatus.Finished);
